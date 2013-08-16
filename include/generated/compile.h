8607ad469a0b19351a32f9b44cbc31a6c8bdd26f
javilonas <admin@lonasdigital.com>
2013-08-16 19:46:20 +0200

    Ptah 0.1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version Stock */
+/* This file is auto generated, version RC2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#Stock SMP PREEMPT Thu Aug 15 09:57:14 CEST 2013"
+#define UTS_VERSION "#RC2 SMP PREEMPT Fri Aug 16 13:50:18 CEST 2013"
 #define LINUX_COMPILE_BY "lonas"
 #define LINUX_COMPILE_HOST "lonas-linux"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
