89b56e43671acd8e70a2bb4d90e714cc0f1bb97c
javilonas <admin@lonasdigital.com>
2013-09-12 09:31:56 +0200

    Revert "Actualizado Boeffla Sound"
    This reverts commit 87896e7d01b252318da58a5b5dc706ffe50ac9b1.

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -40,8 +40,9 @@
 #include "wm8994.h"
 #include "wm_hubs.h"
 
+#ifdef CONFIG_SND_BOEFFLA
 #include "boeffla_sound.h"
-
+#endif
 
 #define WM1811_JACKDET_MODE_NONE  0x0000
 #define WM1811_JACKDET_MODE_JACK  0x0100
@@ -203,7 +204,9 @@ static int wm8994_write(struct snd_soc_codec *codec, unsigned int reg,
 
 	BUG_ON(reg > WM8994_MAX_REGISTER);
 
+#ifdef CONFIG_SND_BOEFFLA
   value = Boeffla_sound_hook_wm8994_write(reg, value);
+#endif
 
 	if (!wm8994_volatile(codec, reg)) {
 		ret = snd_soc_cache_write(codec, reg, value);
@@ -4220,8 +4223,9 @@ static int wm8994_codec_probe(struct snd_soc_codec *codec)
 		break;
 	}
 
+#ifdef CONFIG_SND_BOEFFLA
   Boeffla_sound_hook_wm8994_pcm_probe(codec);
-
+#endif
 	return 0;
 
 err_irq:
